abe58207bf76a2d4c32662fe3c50468f879c0a78
Grubysamuraj <jakub.dragosz@o2.pl>
2023-12-09 12:30:40 +0100

    firebase half-completed

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { View, Text } from "react-native";
 
 export default function Home() {
+  useEffect(()=>{
+    
+  })
   return (
     <View>
       <Text>Do zaimplementowania</Text>
