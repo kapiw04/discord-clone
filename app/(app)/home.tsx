c490a7b2bb5d4564fbd3e95a8311f4684bcab5e9
Grubysamuraj <jakub.dragosz@o2.pl>
2023-12-09 12:59:29 +0100

    idk

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,10 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { getDocs,  collection } from "firebase/firestore";
+import { firestoreDB } from "../../firebaseConfig";
+
+interface serverData  {
+  message:string,
+  username:string
+}
+
 
 export default function Home() {
+  // const docRef = doc(firestoreDB, "rooms","Test2");
+  const docRef = collection(firestoreDB, "rooms");
+  // const [servers,setServers] = useState([]);
+  const [serversId,setServersId] = useState([] as string[]);
+  
   useEffect(()=>{
-    
-  })
+    const getData = async()=>{
+      const {docs} = await getDocs(docRef)
+      setServersId(docs.map(({id})=> id) as any);
+      let temp:string[] = []
+      docs.map(({id})=>{
+        temp.push(id);
+      });
+      setServersId(temp);
+    }
+    getData();
+  },[])
   return (
     <View>
       <Text>Do zaimplementowania</Text>
